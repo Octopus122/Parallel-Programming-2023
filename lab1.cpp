84341fcc1cffb04bb058e8922b4cb5e7f4089cc4
PolinaT <frozen.mind070@gmail.com>
2023-09-19 21:44:45 +0300

    всё коммитим

--- lab1.cpp
+++ lab1.cpp
@@ -1,13 +1,29 @@
-#include <iostream>
-// #include <string>
-
-using std::cin;
-using std::cout;
-
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<thread>
 
 int main(){
-    int name;
-    cout<<"Hello";
-    
+    std::string filename = "sourse/cat1.jpg";
+    std::ifstream f;
+    f.open(filename, std::ifstream::binary);
+    std::vector<char> F;
+    if (f){
+        char s;
+        for(f >> s; !f.eof(); f >> s){
+            F.push_back(s);
+        }
+    }
+    f.close();
+    std::ofstream f1;
+    f1.open("output", std::ifstream::binary);
+    int x = 5;
+    for(int i = 0; i<F.size();++i){
+        F[i] = (i*x)&255;
+        f1<<F[i];
+    }
+    f1.close();
+
     return 0;
 }
